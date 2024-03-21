8fd72744949248b2e90380adbcd030174be1888e
hljeon-it <it.hljeon@gmail.com>
2024-03-21 21:41:30 +0900

    commit

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Question
 
 # Create your views here.
@@ -11,3 +12,8 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)  # question_id가 없을 때 서버오류(500)이 아닌 Not Found(404) 뜨도록 함
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):   # 텍스트창에 입력한 내용은 request 객체를 통해 읽을 수 있음
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())   # 답변 생성: POST로 전송된 폼 데이터 중 content값, 시간
+    return redirect('pybo:detail', question_id=question.id)
